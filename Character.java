2ec34fa4ce6a46974f1d9c0c8f1c5824380eb7d4
Korix <michaelcrowley@acm.org>
2019-04-07 08:04:31 -0400

    Fleshed out base for Character

--- Character.java
+++ Character.java
@@ -4,7 +4,42 @@
  * 
  *  This class dictates how all characters sould be set up.
  */
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import java.awt.Rectangle;
+public class Character{
+  int health;
+  double speed;
+  Rectangle hitbox;
+  int power;
+  int dimension_x;
+  int dimension_y;
+  private Image currImage;
+  private ImageIcon[] idle_anim;
+  private ImageIcon[] death_anim;
+  private ImageIcon[] attack_anim;
 
-public interface Character{
-  
+  public Character(){
+    health = 1;
+    speed = 5;
+    dimension_x = 20;
+    dimension_y = 80;
+    hitbox = new Rectangle(0, 0, dimension_x, dimension_y)
+    power = 1;
+
+  }
+
+  private boolean healtIsGone(){
+    return (health<=0);
+  }
+
+  private void takeDamage(){
+    health--;
+    if(healtIsGone())
+      die();
+  }
+
+  private die(){
+    
+  }
 }
