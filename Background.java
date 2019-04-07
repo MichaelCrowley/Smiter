d04e4bd9cbbc9cc9b665fb2c3695c9d834e6c314
Korix <michaelcrowley@acm.org>
2019-04-06 22:19:40 -0400

    Refactored for better naming conventions

--- Background.java
+++ Background.java
@@ -6,8 +6,6 @@
  */
 
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.Color;
 import java.awt.Toolkit;
 import java.awt.Image;
 import javax.swing.ImageIcon;
@@ -15,20 +13,20 @@ import javax.swing.JPanel;
 
 public class Background extends JPanel{
 
-  private Image p;
+  private Image background;
 
   public Background(){
-    loadImage();
+    loadBackground();
   }
 
-  //Display images
+  //Display image
   public void draw(Graphics2D g2){
     g2.drawImage(p, 200, 200, this);
-      Toolkit.getDefaultToolkit().sync();
+    Toolkit.getDefaultToolkit().sync();
   }
 
-  private void loadImage(){    
-    ImageIcon ii = new ImageIcon("assets/player/adventurer-idle-00.png");
-    p = ii.getImage();
+  private void loadBackground(){    
+    ImageIcon source = new ImageIcon("assets/player/adventurer-idle-00.png");
+    background = source.getImage();
   }
 }
